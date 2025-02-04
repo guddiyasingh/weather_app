5f7c52005f02211e15c081e3d7197b8d2ee27689
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-02-04 13:36:06 +0530

    add images

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import './Weather.css'
+import search_icon from '../assets/search.png';
+import cloudy_icon from '../assets/cloudy.png'
 
 const Weather = () => {
   return (
-    <div>
-      <h1>Weather</h1>
+    <div className='weather'>
+      <div className='search-bar'>
+        <input type="text"  placeholder='search'/>
+        <img src={search_icon} alt="" />
+        <img src={cloudy_icon} alt="" />
+      </div>
+     
     </div>
   )
 }
